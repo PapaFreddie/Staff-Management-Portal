be8a7452b906799017ad57d7f190938264d17551
PapaFreddie <fred.papa@student.moringaschool.com>
2022-03-24 10:31:40 +0300

    added api for department

--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from staff.models import Departments, Employees
+from staff.serializers import DepartmentSerializer, Employees
 
 # Create your views here.
+@csrf_exempt
+def departmentApi(request)
